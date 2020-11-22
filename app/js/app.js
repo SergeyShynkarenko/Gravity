4db1250d14611cba6600e8bfc0c7984853655f81
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2020-11-22 20:23:26 +0300

    services section

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-	// Custom JS
-
+$(function() {
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:true,
+	    dots: false,
+	    navText: ['<img src="images/dest/left.svg" alt="">', '<img src="images/dest/right.svg" alt="">'],
+	    startPosition: 1,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
 });
