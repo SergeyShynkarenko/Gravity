016d23ccf240b0f861353f3e8cc872e71082ea1f
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2020-11-23 20:07:48 +0300

    section testimonials

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,31 @@
 $(function() {
-	$('.owl-carousel').owlCarousel({
+	$('.works-carousel').owlCarousel({
 	    loop:true,
 	    margin:30,
 	    nav:true,
 	    dots: false,
 	    navText: ['<img src="images/dest/left.svg" alt="">', '<img src="images/dest/right.svg" alt="">'],
 	    startPosition: 1,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
+
+		$('.contributor-carousel').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:true,
+	    dots: false,
+	    navText: ['<img src="images/dest/left.svg" alt="">', '<img src="images/dest/right.svg" alt="">'],
+	    // startPosition: 1,
 	    responsive:{
 	        0:{
 	            items:1
